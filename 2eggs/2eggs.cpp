6a9ee0f955a07d222031e6b70efb54568ac91091
Kanathuch Siribannakul <131222639+Waterforces@users.noreply.github.com>
2024-06-29 22:21:56 +0700

    Commits 1
    TC 3,4,5

--- 2eggs/2eggs.cpp
+++ 2eggs/2eggs.cpp
@@ -1,9 +1,17 @@
 #include "2eggs.h"
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-
-int height_threshold(int N, int Q) {
-  
-  return 1;
+int height_threshold(int N, int Q)
+{
+    for(int i=Q,h=Q-1;i<=N;i+=h,h--) {
+        if(drop_egg(1,i)) {
+            for(int j=i-h;j<i;j++) {
+                if(drop_egg(2,j)) return j;
+            }
+            return i;
+        }
+    }
+    return N;
 }
